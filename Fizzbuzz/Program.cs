ff6b377fda8e1cbd430b84368d00256aab1ccce8
root <root@U-AA-HXH91T3-L>
2023-10-10 13:40:03 +0100

    Initial commit

---
+++ Fizzbuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿for (int x = 1; x <= 100; x++) 
+{ 
+    string result = "";
+
+    if (x % 3 == 0) 
+    {
+        result += "Fizz";
+    }
+
+    if (x % 5 == 0) 
+    {
+        result += "Buzz";
+    }
+
+    if (x % 7 == 0) 
+    {
+        result += "Bang";
+    }
+
+    if (x % 13 == 0) 
+    {
+        result += "Fezz";
+    }
+
+    if (x % 11 == 0) 
+    {
+        result = "Bong";
+        
+    }
+
+
+    if (result.Length == 0)
+    {
+        Console.WriteLine(x);
+    }
+    else
+    {
+        Console.WriteLine(result);
+    }
+}
