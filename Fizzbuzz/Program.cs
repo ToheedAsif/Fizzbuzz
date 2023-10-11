3ce8074b8ff494446a264efc9f9729d4da0c3fd0
root <root@U-AA-HXH91T3-L>
2023-10-11 13:10:32 +0100

    Added the extended fizzbuzz

--- Fizzbuzz/Program.cs
+++ Fizzbuzz/Program.cs
@@ -1,40 +1,76 @@
-﻿for (int x = 1; x <= 100; x++) 
-{ 
-    string result = "";
+﻿using System;
+using System.Collections.Generic;
 
-    if (x % 3 == 0) 
+class Program
+{
+    static void Main()
     {
-        result += "Fizz";
+        for (int x = 1; x <= 300; x++)
+        {
+            List<string> results = new List<string>();
+
+            if (IsDivisibleBy(x, 3))
+            {
+                results.Add("Fizz");
+            }
+
+            if (IsDivisibleBy(x, 5))
+            {
+                results.Add("Buzz");
+            }
+
+            if (IsDivisibleBy(x, 7))
+            {
+                results.Add("Bang");
+            }
+
+            if (IsDivisibleBy(x, 11))
+            {
+                results.Clear();
+                results.Add("Bong");
+            }
+
+            if (IsDivisibleBy(x, 13))
+            {
+                bool inserted = false;
+                for (int i = 0; i < results.Count; i++)
+                {
+                    if (results[i].StartsWith("B"))
+                    {
+                        results.Insert(i, "Fuzz");
+                        inserted = true;
+                        break;
+                    }
+                }
+                if (!inserted)
+                {
+                    results.Add("Fuzz");
+                }
+            }
+
+            if (IsDivisibleBy(x, 17))
+            {
+                results.Reverse();
+            }
+
+            if (results.Count == 0)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", results));
+            }
+
+            if (IsDivisibleBy(x, 17))
+            {
+                results.Reverse();
+            }
+        }
     }
 
-    if (x % 5 == 0) 
+    static bool IsDivisibleBy(int numerator, int denominator)
     {
-        result += "Buzz";
-    }
-
-    if (x % 7 == 0) 
-    {
-        result += "Bang";
-    }
-
-    if (x % 13 == 0) 
-    {
-        result += "Fezz";
-    }
-
-    if (x % 11 == 0) 
-    {
-        result = "Bong";
-        
-    }
-
-
-    if (result.Length == 0)
-    {
-        Console.WriteLine(x);
-    }
-    else
-    {
-        Console.WriteLine(result);
+        return numerator % denominator == 0;
     }
 }
